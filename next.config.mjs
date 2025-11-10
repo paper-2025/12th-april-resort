a82523f0fd520d0fb2e6e0737ba10838f1e0aa47
paper-2025 <desmondathisbest@gmail.com>
2025-11-10 23:38:41 +0300

    third

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 👇 force Webpack instead of Turbopack
-  experimental: {
-    turbo: false,
-  },
+  
 };
 
 export default nextConfig;
